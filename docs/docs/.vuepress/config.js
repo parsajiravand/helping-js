f14b458b77d1d99626efdfed35bc18e61949ca35
parsajiravand <parsajiravand@gmail.com>
2024-07-21 17:40:39 +0330

    Add Phonenumber and currency regex links

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -2,15 +2,14 @@
 import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
 import { path } from "@vuepress/utils";
 import { defaultTheme } from "vuepress";
-import { mediumZoomPlugin } from '@vuepress/plugin-medium-zoom'
+import { mediumZoomPlugin } from "@vuepress/plugin-medium-zoom";
 export default {
   theme: defaultTheme({
     locales: {
       "/": {
         lang: "en-US",
         title: "Vue Client Recaptcha",
-        description:
-          "Build simple recaptcha for vuejs without need server .",
+        description: "Build simple recaptcha for vuejs without need server .",
       },
     },
     logo: "https://i.ibb.co/7RkRvX0/helping-js-icon.png",
@@ -34,7 +33,7 @@ export default {
           },
         ],
       },
-       // NavbarItem
+      // NavbarItem
       {
         text: "Npm",
         link: "https://www.npmjs.com/package/helping-js",
@@ -55,23 +54,30 @@ export default {
         link: "/support/",
       },
       {
-        text: "SafeType",
+        text: "Safe Type",
         link: "/safe-type/",
       },
       {
-        text: "RegexHelper",
+        text: "Regex Helper",
         link: "/regex/",
-      }
+      },
+      {
+        text: "PhoneNumbers Regex Helper",
+        link: "/phone-regex/",
+      },
+      {
+        text: "Currency Regex Helper",
+        link: "/currency-regex/",
+      },
     ],
   }),
   plugins: [
     registerComponentsPlugin({
-
       componentsDir: path.resolve(__dirname, "./components"),
     }),
     mediumZoomPlugin({
       // options
-      selector:':not(a) > s'
+      selector: ":not(a) > s",
     }),
   ],
 };
